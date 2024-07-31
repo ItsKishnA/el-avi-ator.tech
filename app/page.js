67a5d8dcf73040e1a2cbb55ddac0a6ef40d7f294
ItsKishnA <kishnayc@gmail.com>
2024-07-31 23:01:30 +0530

    First upload

--- app/page.js
+++ app/page.js
@@ -1,113 +1,308 @@
+"use client";
+
+import About from "@/component/About";
 import Image from "next/image";
+import Link from "next/link";
+import React, { useState, StyleSheet } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const mehh = {
+    name: "Avi Mahajan",
+    title: "Software Developer",
+    location: "Indore, India",
+  };
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const education = {
+    college: {
+      name: "Medi-Caps University",
+      degree: "Bachelor of Technology (B.Tech)",
+      major: "Computer Science & Engineering (CSE)",
+      year: "2021-2025 (Ongoing)",
+    },
+    school: {
+      name: "Sophia Convent School",
+      degree: "Senior Secondary School",
+      major: "Science",
+      location: "Khandwa",
+      // year: "2017-2019",
+    },
+  };
+
+  const projects = {
+    portfolio: {
+      name: "Portfolio",
+      description: "Personal Portfolio Website",
+      tech_stack: ["Next.js", "Tailwind CSS"],
+      // github:"",
+    },
+    memo_game: {
+      name: "Memo Game",
+      description: "A simple memo game",
+      tech_stack: ["React Native", "JavaScript", "Expo"],
+      // github:"",
+    },
+    firebase_auth: {
+      name: "Firebase Auth",
+      description: "Firebase Authentication",
+      tech_stack: ["React Native", "Firebase"],
+      // github:"",
+    },
+  };
+
+  const Project = ({ project }) => {
+    return (
+      <div className="p-5 bg-slate-900">
+        <h3 className="text-2xl font-bold text-red-600">{project.name}</h3>
+        <div className="flex flex-col ">A react native memo game</div>
+      </div>
+    );
+  };
+
+  const Projects = () => {
+    return (
+      <div id="PROJECTS" className="p-5">
+        <h2 className="text-3xl font-bold leading-loose">Projects</h2>
+        <Project project={projects.memo_game} />
+      </div>
+    );
+  };
+
+  const ContactMe = () => {
+    const iconSize = 50;
+
+    const Icon = ({ iconName }) => {
+      let color;
+      let viewBoxValue;
+      let pathD;
+
+      if (iconName === "gmail") {
+        color = null;
+        viewBoxValue = "0 0 48 48";
+        pathD = [
+          {
+            fill: "#4caf50",
+            d: "M45,16.2l-5,2.75l-5,4.75L35,40h7c1.657,0,3-1.343,3-3V16.2z",
+          },
+          {
+            fill: "#1e88e5",
+            d: "M3,16.2l3.614,1.71L13,23.7V40H6c-1.657,0-3-1.343-3-3V16.2z",
+          },
+          {
+            fill: "#e53935",
+            d: "M35,11.2 24,19.45 13,11.2 12,17 13,23.7 24,31.95 35,23.7 36,17",
+          },
+          {
+            fill: "#c62828",
+            d: "M3,12.298V16.2l10,7.5V11.2L9.876,8.859C9.132,8.301,8.228,8,7.298,8h0C4.924,8,3,9.924,3,12.298z",
+          },
+          {
+            fill: "#fbc02d",
+            d: "M45,12.298V16.2l-10,7.5V11.2l3.124-2.341C38.868,8.301,39.772,8,40.702,8h0C43.076,8,45,9.924,45,12.298z",
+          },
+        ];
+      } else if (iconName === "linkedin") {
+        color = "#0077B5";
+        viewBoxValue = "0 0 50 50";
+        pathD =
+          "M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z";
+      } else if (iconName === "github") {
+        color = "#fff";
+        viewBoxValue = "0 0 64 64";
+        pathD =
+          "M32 6C17.641 6 6 17.641 6 32c0 12.277 8.512 22.56 19.955 25.286-.592-.141-1.179-.299-1.755-.479V50.85c0 0-.975.325-2.275.325-3.637 0-5.148-3.245-5.525-4.875-.229-.993-.827-1.934-1.469-2.509-.767-.684-1.126-.686-1.131-.92-.01-.491.658-.471.975-.471 1.625 0 2.857 1.729 3.429 2.623 1.417 2.207 2.938 2.577 3.721 2.577.975 0 1.817-.146 2.397-.426.268-1.888 1.108-3.57 2.478-4.774-6.097-1.219-10.4-4.716-10.4-10.4 0-2.928 1.175-5.619 3.133-7.792C19.333 23.641 19 22.494 19 20.625c0-1.235.086-2.751.65-4.225 0 0 3.708.026 7.205 3.338C28.469 19.268 30.196 19 32 19s3.531.268 5.145.738c3.497-3.312 7.205-3.338 7.205-3.338.567 1.474.65 2.99.65 4.225 0 2.015-.268 3.19-.432 3.697C46.466 26.475 47.6 29.124 47.6 32c0 5.684-4.303 9.181-10.4 10.4 1.628 1.43 2.6 3.513 2.6 5.85v8.557c-.576.181-1.162.338-1.755.479C49.488 54.56 58 44.277 58 32 58 17.641 46.359 6 32 6zM33.813 57.93C33.214 57.972 32.61 58 32 58 32.61 58 33.213 57.971 33.813 57.93zM37.786 57.346c-1.164.265-2.357.451-3.575.554C35.429 57.797 36.622 57.61 37.786 57.346zM32 58c-.61 0-1.214-.028-1.813-.07C30.787 57.971 31.39 58 32 58zM29.788 57.9c-1.217-.103-2.411-.289-3.574-.554C27.378 57.61 28.571 57.797 29.788 57.9z";
+      } else if (iconName === "twitter") {
+        color = "#fff";
+        viewBoxValue = "0 0 48 48";
+        pathD =
+          "M11 4C7.134 4 4 7.134 4 11v32c0 2.866 3.134 5 7 5h28c2.866 0 5-2.134 5-5V11c0-2.866-2.134-7-5-7H11zM13.085938 13L21.023438 13 26.660156 21.009766 33.5 13 36 13 27.789062 22.613281 37.914062 37 29.978516 37 23.4375 27.707031 15.5 37 13 37 22.308594 26.103516 13.085938 13zM16.914062 15L31.021484 35 34.085938 35 19.978516 15 16.914062 15z";
+      }
+
+      return (
+        <svg
+          width={iconSize}
+          height={iconSize}
+          viewBox={viewBoxValue}
+          style={{ fill: color }}
+        >
+          {Array.isArray(pathD) ? (
+            pathD.map((path, index) => (
+              <path key={index} fill={path.fill} d={path.d} />
+            ))
+          ) : (
+            <path d={pathD} />
+          )}
+        </svg>
+      );
+    };
+
+    const ContactCard = ({ cardName, title, toCopy, toLink }) => {
+      const [showCopied, setShowCopied] = useState(false);
+
+      const handleClick = () => {
+        navigator.clipboard.writeText(toCopy);
+        setShowCopied(true);
+        setTimeout(() => {
+          setShowCopied(false);
+        }, 3000);
+      };
+
+      return (
+        <div className="flex flex-col p-4">
+          <h3 className="text-2xl text-gray-600">{title}</h3>
+
+          <div className="flex justify-between">
+            <Link href={toLink} target="_blank">
+              <Icon iconName={cardName} />
+            </Link>
+
+            {/* <button
+              className="flex items-center bg-gray-800 px-5 m-2 rounded-full text-sm text-gray-400"
+              onClick={() => navigator.clipboard.writeText(toCopy)}
+            >
+              {toCopy}
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                style={{ fill: "white", marginLeft: "8px" }}
+              >
+                <path d="M7 5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-2v2a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h2zm2 2h5a3 3 0 0 1 3 3v5h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-9a1 1 0 0 0-1 1zM5 9a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h9a1 1 0 0 0 1-1v-9a1 1 0 0 0-1-1z" />
+              </svg>
+            </button> */}
+
+            <button
+              className="flex items-center bg-gray-800 px-5 m-2 rounded-full text-sm text-gray-400"
+              onClick={handleClick}
+            >
+              {toCopy}
+              {showCopied ? (
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  style={{ fill: "white", marginLeft: "8px" }}
+                >
+                  <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm-1 15-5-5 1.41-1.41L11 14.17l7.59-7.59L20 8z" />
+                </svg>
+              ) : (
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  style={{ fill: "white", marginLeft: "8px" }}
+                >
+                  <path d="M7 5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-2v2a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h2zm2 2h5a3 3 0 0 1 3 3v5h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-9a1 1 0 0 0-1 1zM5 9a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h9a1 1 0 0 0 1-1v-9a1 1 0 0 0-1-1z" />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
+      );
+    };
+    return (
+      <div className="p-4 flex flex-col">
+        <h2 className="text-3xl font-bold leading-loose">Contact Me</h2>
+
+        <ContactCard
+          cardName="linkedin"
+          title={
+            <span>
+              Follow me on <b className="text-white">LinkedIn</b>
+            </span>
+          }
+          toLink={"https://www.linkedin.com/in/avi-mahajan2401/"}
+          toCopy={"avi-mahajan2401"}
+        />
+
+        <ContactCard
+          cardName="twitter"
+          title={
+            <span>
+              Connect me on <b className="text-white">X</b>(Twitter)
+            </span>
+          }
+          toLink={"#"}
+          toCopy={"@avi_mahajan"}
+        />
+
+        <ContactCard
+          cardName="github"
+          title={
+            <span>
+              Connect me on <b className="text-white">GitHub</b>
+            </span>
+          }
+          toLink={"https://www.github.com/ItsKishnA"}
+          toCopy={"ItsKishnA"}
+        />
+
+        <ContactCard
+          cardName="gmail"
+          title={
+            <span>
+              Write me a <b className="text-white">Mail</b>
+            </span>
+          }
+          toLink={"#"}
+          toCopy={"avi.knw@gmail.com"}
         />
       </div>
+    );
+  };
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+  const MessageMe = () => {
+    return (
+      // <div id="MESSAGE" className="flex flex-col flex-1 p-4">
+      <div id="MESSAGE" className="w-full p-4 h-2/3 flex flex-col">
+        <h2 className="text-3xl font-bold leading-loose">Drop a message</h2>
+        <form className="flex flex-col flex-1">
+          <div className="flex flex-col flex-1 md:flex-row">
+            <input
+              type="text"
+              placeholder="First Name"
+              className="p-2 m-2 flex-1"
+              required
+            />
+            <input
+              type="text"
+              placeholder="Last Name "
+              className="p-2 m-2 flex-1"
+              required
+            />
+          </div>
+          <input
+            type="email"
+            placeholder="Email"
+            className="p-2 m-2 flex-1 text-gray-600 aligh-top"
+            required
+          />
+          <textarea placeholder="Message" className="p-2 m-2 flex-1" required />
+          <button type="submit" className="p-2 m-2">
+            Send
+          </button>
+        </form>
       </div>
+    );
+  };
+
+  const ContactPage = () => {
+    return (
+      <div className="flex flex-col md:flex-row w-full md:h-full">
+        <div className="md:flex flex-[1] items-center p-4">
+          <MessageMe />
+        </div>
+        <div className="flex-[1] p-4 flex justify-center">
+          <ContactMe />
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <main className="min-h-screen flex items-center">
+      <About />
+      {/* <Projects /> */}
+      {/* <ContactPage /> */}
     </main>
   );
 }
---
+++ component/About.js
@@ -0,0 +1,86 @@
+import Image from "next/image";
+
+const Description = () => {
+  return (
+    <div className="flex pr-10">
+      <p className="text-white leading-relaxed">
+        <b style={styles.firstLetter}>A</b>s a{" "}
+        <b style={styles.firstLetter}>B</b>
+        rilliant <b style={styles.firstLetter}>C</b>
+        omputer science <b style={styles.firstLetter}>D</b>
+        evotee, <b style={styles.firstLetter}>E</b>
+        xuding <b style={styles.firstLetter}>F</b>ervor for learning, I find joy
+        in <b style={styles.firstLetter}>G</b>
+        rasping concepts with ease. <b style={styles.firstLetter}>H</b>aving an{" "}
+        <b style={styles.firstLetter}>I</b>nquisitive and{" "}
+        <b style={styles.firstLetter}>J</b>
+        ovial spirit, I seek <b style={styles.firstLetter}>K</b>nowledge
+        relentlessly. <b style={styles.firstLetter}>L</b>ifelong learning fuels
+        my <b style={styles.firstLetter}>M</b>otivated journey.{" "}
+        <b style={styles.firstLetter}>N</b>urturing an{" "}
+        <b style={styles.firstLetter}>O</b>
+        ptimistic, <b style={styles.firstLetter}>P</b>ositive, and{" "}
+        <b style={styles.firstLetter}>Q</b>uick-to-adapt mindset, I stand as a{" "}
+        <b style={styles.firstLetter}>R</b>emarkable{" "}
+        <b style={styles.firstLetter}>S</b>
+        tudent. <b style={styles.firstLetter}>T</b>ireless,{" "}
+        <b style={styles.firstLetter}>U</b>
+        nwavering, and <b style={styles.firstLetter}>V</b>ersatile, I{" "}
+        <b style={styles.firstLetter}>X</b>cel in creativity.{" "}
+        <b style={styles.firstLetter}>W</b>ith a{" "}
+        <b style={styles.firstLetter}>Y</b>
+        earning for growth, I remain a <b style={styles.firstLetter}>Z</b>
+        ealous explorer of knowledge.
+      </p>
+    </div>
+  );
+};
+
+const DownloadButton = () => {
+  return (
+    <a
+      href={"/resume/resume.pdf"}
+      target="_blank"
+      download={"Avi's_Resume.pdf"}
+    >
+      <button type="button">Download Resume</button>
+    </a>
+  );
+};
+
+const About = () => {
+  return (
+    <div
+      id="ABOUT"
+      style={{ width: "75%" }}
+      className="p-5 flex flex-col-reverse flex-1 md:flex-row"
+    >
+      <div className=" flex-[2] md:flex-[3]">
+        <h1 className="text-3xl font-bold leading-loose">About Mehhh...</h1>
+        <Description />
+        <DownloadButton />
+      </div>
+
+      <div className="flex-[1] content-center md:flex[2]">
+        <Image
+          src="/images/avatar.jpg"
+          width={200}
+          height={200}
+          alt="Picture of the author"
+          className="rounded-full transform-gpu w-full"
+        />
+      </div>
+    </div>
+  );
+};
+
+const styles = {
+  firstLetter: {
+    color: "#e50914",
+    fontSize: "1.5rem",
+    // lineHeight: "2rem",
+    fontWeight: "900",
+  },
+};
+
+export default About;
