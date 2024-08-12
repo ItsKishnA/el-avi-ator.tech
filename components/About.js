5786ac8dfd881a8f909767d11242787c25906dbb
ItsKishnA <kishnayc@gmail.com>
2024-08-12 17:42:52 +0530

    Download Resume Button Customised

--- components/About.js
+++ components/About.js
@@ -28,11 +28,50 @@ const Description = () => {
 
 const DownloadButton = () => {
   return (
-    <div className="bg-red-600 p-2 rounded-full px-5 items-center justify-center">
-      <a href={"/resume/avi_resume.pdf"} target="_blank" download>
-        <button type="button">Download Resume</button>
-      </a>
-    </div>
+    // <div className="bg-gray-400 p-2 rounded-full px-5 items-center justify-center">
+    <a
+      href={"/resume/avi_resume.pdf"}
+      target="_blank"
+      download
+      className="flex items-center justify-center rounded-full p-2 px-5"
+    >
+      <div className="flex flex-row">
+        <div className="flex justify-center items-center whitespace-nowrap">
+          <h5 className="text-gray-600 bg-white text-xs font-mono font-bold p-2 px-4 pr-8 translate-x-6 rounded-l-2xl">
+            Download Resume
+          </h5>
+        </div>
+        <div className="rounded-full bg-red-600 animate-pulse-red p-6">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <path
+                opacity="0.5"
+                d="M3 15C3 17.8284 3 19.2426 3.87868 20.1213C4.75736 21 6.17157 21 9 21H15C17.8284 21 19.2426 21 20.1213 20.1213C21 19.2426 21 17.8284 21 15"
+                stroke="#212121"
+                stroke-width="2.136"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>{" "}
+              <path
+                d="M12 3V16M12 16L16 11.625M12 16L8 11.625"
+                stroke="#212121"
+                stroke-width="2.136"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>{" "}
+            </g>
+          </svg>
+        </div>
+      </div>
+    </a>
+    // </div>
   );
 };
 
