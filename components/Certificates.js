c92b20a2bfcf79296b711a212fcb9a93f2d339be
ItsKishnA <kishnayc@gmail.com>
2024-10-16 17:13:28 +0530

    Removed Animation

--- components/Certificates.js
+++ components/Certificates.js
@@ -2,15 +2,24 @@ import Image from "next/image";
 
 const Certificates = () => {
   return (
-    <div className="boxed p-2">
+    <div className="boxed p-2 flex justify-center flex-col items-center">
       <Image
         src="/certificates/pm_pmmi.jpg"
         alt="Certificates"
-        height={400}
-        width={400}
-        className="rounded-xl"
+        height={200}
+        width={200}
+        className="rounded-xl w-[100%] h-[100%] object-cover"
         style={{ objectFit: "contain" }}
+        loading="lazy"
       />
+      <div className="bg-black -translate-y-[16px] bg-opacity-50 rounded-b-lg px-5 w-[100%] pt-[20px]">
+        <p className="font-mono flex-wrap font-bold">
+          Project Management Foundations Certification
+        </p>
+        <p className="font-mono flex-wrap text-gray-600 font-normal">
+          LinkedIn Learning
+        </p>
+      </div>
     </div>
   );
 };
--- components/TechStack.js
+++ components/TechStack.js
@@ -6,18 +6,10 @@ import loadedData from "../public/data/blockContent.json";
 
 const About = () => {
   return (
-    <div className="flex flex-col h-full w-80 items-center boxed p-4">
-      <div className="flex flex-col md:flex-[1] items-start content-start gap-2 overflow-hidden animatedSlider">
+    <div className="flex flex-col h-full items-center boxed p-4">
+      <div className="flex flex-col md:flex-[1] items-start content-start gap-2">
         <h1 className="text-xl font-bold leading-tight">Tech Stack</h1>
-        <div className=" flex flex-row animatedSliderContent gap-[8px]">
-          {loadedData.data.techStack.map((tech, id) => (
-            <img
-              src={tech.src}
-              key={id}
-              alt={tech.name}
-              className="transform-gpu w-[50px] h-[50px]"
-            />
-          ))}
+        <div className=" flex flex-row flex-wrap gap-[8px] justify-center">
           {loadedData.data.techStack.map((tech, id) => (
             <img
               src={tech.src}
