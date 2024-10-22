9f27fd1046eb17597fd6fbdb6e29fcb31e3e966f
ItsKishnA <kishnayc@gmail.com>
2024-10-22 18:43:23 +0530

    ChatApp images added

--- components/ChatApp.js
+++ components/ChatApp.js
@@ -4,7 +4,7 @@ const ChatApp = () => {
       <div className="flex flex-col gap-2 items-center">
         <div className="w-full flex justify-start">
           <img
-            src="https://via.placeholder.com/160x90"
+            src="./images/Screenshot_1729539343.png"
             alt="Chat App (1)"
             className=" rounded-xl"
             width={213}
@@ -14,7 +14,7 @@ const ChatApp = () => {
         </div>
         <div className="w-full flex justify-end">
           <img
-            src="https://via.placeholder.com/160x90"
+            src="./images/Screenshot_1729539565.png"
             alt="Chat App (2)"
             className="rounded-xl bg-slate-500"
             width={213}
@@ -24,7 +24,12 @@ const ChatApp = () => {
         </div>
       </div>
       <div className="flex flex-col mt-2  flex-wrap">
-        <h1 className="font-bold text-lg">Chat App</h1>
+        <div className="flex flex-row items-center justify-between">
+          <h1 className="font-bold text-lg">Chat App</h1>
+          <a href="">
+            <h3 className="font-mono text-md text-gray-100 underline">Code</h3>
+          </a>
+        </div>
         <p className="text-gray-400 font-mono">
           A simple chat application built using ReactJS
         </p>
